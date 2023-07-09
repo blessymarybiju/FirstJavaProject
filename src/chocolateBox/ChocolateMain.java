9909fa75f69274d1fe062e3473ac3d2024aff111
blessymarybiju <nishabiju14@gmail.com>
2023-07-09 00:31:57 -0400

    Assignment question 4

---
+++ src/chocolateBox/ChocolateMain.java
@@ -0,0 +1,12 @@
+package chocolateBox;
+
+public class ChocolateMain {
+
+	public static void main(String[] args) {
+		Chocolate c = new Chocolate();
+		c.noOfChocolates = 27;
+		c.addingChocolatesToBox();
+
+	}
+
+}
