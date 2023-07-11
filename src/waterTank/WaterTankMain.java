8507d38c14995964d23f8234d17be5ff20a4f227
blessymarybiju <nishabiju14@gmail.com>
2023-07-10 22:47:56 -0400

    Updated package waterTank

--- src/waterTank/WaterTankMain.java
+++ src/waterTank/WaterTankMain.java
@@ -4,7 +4,6 @@ public class WaterTankMain {
 
 	public static void main(String[] args) {
 		WaterTankCapacity wtc = new WaterTankCapacity();
-		//test
 		wtc.capacity = 0.0;
 		wtc.bucket = 10.00;
 		wtc.maxCapacity=95.00;
