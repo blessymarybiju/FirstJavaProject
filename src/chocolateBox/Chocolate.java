4a1c4b291959d099298448ab52b211b7c3be1ebd
blessymarybiju <nishabiju14@gmail.com>
2023-07-10 22:40:10 -0400

    Updated package chocolateBox

--- src/chocolateBox/Chocolate.java
+++ src/chocolateBox/Chocolate.java
@@ -3,17 +3,21 @@ package chocolateBox;
 public class Chocolate {
 
 	int noOfChocolates;
+	int maxNumberOfChocolates;
+	int chocolateCount;
 
-	void addingChocolatesToBox() {
+	void addChocolatesToBox() {
 		System.out.println("Number of chocolates in the box before filling: " + noOfChocolates);
-		while (noOfChocolates <= 63) {
-			if (noOfChocolates >= 62) {
+		while (noOfChocolates <= maxNumberOfChocolates) {
+			if (noOfChocolates >= maxNumberOfChocolates) {
 				break;
 			}
-			noOfChocolates += 5;
+			System.out.println("Number of chocolates in the box: " + noOfChocolates);
+			noOfChocolates += chocolateCount;
 
 		}
-		System.out.println("Number of chocolates in the box after filling: " + noOfChocolates);
+		System.out.println("Can't add another " + chocolateCount
+				+ " chocolates to the box. Maximum chocolates fit in the box is " + maxNumberOfChocolates);
 	}
 
 }
--- src/chocolateBox/ChocolateMain.java
+++ src/chocolateBox/ChocolateMain.java
@@ -5,7 +5,9 @@ public class ChocolateMain {
 	public static void main(String[] args) {
 		Chocolate c = new Chocolate();
 		c.noOfChocolates = 27;
-		c.addingChocolatesToBox();
+		c.maxNumberOfChocolates = 63;
+		c.chocolateCount = 5;
+		c.addChocolatesToBox();
 
 	}
 
